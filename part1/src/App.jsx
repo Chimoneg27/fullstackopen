5c59e7c07e1e2c9dfc5f56342f364111d9af38ce
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-09-13 17:33:24 +0200

    learn react props

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,17 +1,28 @@
-function App() {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a +
-     b)
+import PropTypes from 'prop-types';
+
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello world, it {now.toString()}</p>
-      <p>
-      {a} plus {b} is {a + b}
-      </p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
   )
+}
+
+function App() {
+  const name = 'Garvin'
+  const age = 22
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name={name} age={age}/>
+    </div>
+  )
+}
+
+Hello.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired
 }
 
 export default App
