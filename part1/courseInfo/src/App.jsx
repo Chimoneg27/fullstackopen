5d3060dc2d30d06094e7290df218216c0ad7fdc3
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-09-16 16:03:15 +0200

    make smaller components

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,16 +1,41 @@
 import { useState } from 'react'
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   const [counter, setCounter] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+  const decreaseByOne = () => setCounter(counter - 1)
 
   return (
     <div>
-      <div>{counter}</div>
+      <Display counter={counter} />
+      <Button 
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button 
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button 
+        onClick={decreaseByOne}
+        text='minus'
+      />
     </div>
   )
 }
