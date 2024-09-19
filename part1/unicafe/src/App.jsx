794fbf92650c7f02b11bac2432602f564785151c
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-09-20 01:21:13 +0200

    finish unicafe 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,10 +1,39 @@
-// import { useState } from 'react'
+import { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Paragraph = (props) => {
+  return (
+    <p>
+      {props.text} {props.rating}
+    </p>
+  )
+}
 
 function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <h1>Put code here</h1>
+      <h1>give feedback</h1>
+
+      <Button text='good' handleClick={() =>setGood(good + 1)}/>
+      <Button text='neutral' handleClick={() =>setNeutral(neutral + 1)}/>
+      <Button text='bad' handleClick={() =>setBad(bad + 1)}/>
+
+      <h2>statistics</h2>
+
+      <Paragraph text={'good'} rating={good}/>
+      <Paragraph text={'neutral'} rating={neutral}/>
+      <Paragraph text={'bad'} rating={bad}/>
     </div>
   )
 }
