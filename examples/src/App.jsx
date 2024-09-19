1967d5fabf11d293aedaf582db1e5d4f5e086433
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-09-19 16:27:49 +0200

    refactor props

--- examples/src/App.jsx
+++ examples/src/App.jsx
@@ -19,34 +19,30 @@ function App() {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right)
+    setLeft(left + 1)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(updatedRight)
-    setTotal(left + updatedRight)
+    setRight(right + 1)
   }
+
+  const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
 
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClick}>
-        right
-      </button>
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
       {right}
-      <p>{allClicks.join(' ')}</p>
-      <p>Total clicks: {total}</p>
+      <History allClicks={allClicks}/>
     </div>
   )
 }
