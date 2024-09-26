ad7e99513b06a3175de60817f6ab0057931640a0
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-09-27 01:10:02 +0200

    copy code from part1

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercise: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercise: 7
+      },
+      {
+        name: "State of a component",
+        exercise: 14
+      }
+    ]
+  }
+
+  const Header = (props) => {
+    return <h1>{props.course.name}</h1>
+  }
+
+  const Part = ({name, number}) => {
+    return (
+      <p>
+        {name} {number}
+      </p>
+    )
+  }
+
+  const Content = (props) => {
+    return (
+      <div>
+        <Part name={props.parts[0].name} number={props.parts[0].exercise} />
+        <Part name={props.parts[1].name} number={props.parts[1].exercise} />
+        <Part name={props.parts[2].name} number={props.parts[2].exercise} />
+      </div>
+    )
+  }
+
+  const Total = (props) => {
+    return (
+      <p>
+        Number of exercises{" "}
+        {props.parts[0].exercise + props.parts[1].exercise + props.parts[2].exercise}
+      </p>
+    )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
   )
 }
 
