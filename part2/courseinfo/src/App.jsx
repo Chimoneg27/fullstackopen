25e1b020dc41b74331e37ed63cec8b8da0a103a6
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-09-27 21:54:18 +0200

    use course components

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,24 +1,4 @@
-const Course = ({ course }) => {
-  return (
-    <div>
-    {course.map((program) => {
-      return (
-        <>
-        <h2 key={program.id}>{program.name}</h2>
-        
-        <ul>
-          {program.parts.map((lesson) => {
-            return <li key={lesson.id}>{lesson.name} {lesson.exercises}</li>
-          })}
-        </ul>
-
-        <p>total of {program.parts.reduce((total, num) => total + num.exercises,0)} exercises</p>
-        </>
-      )
-    })}
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
  const courses = [
