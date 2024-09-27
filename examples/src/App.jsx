408070fc8d79dc5bb1ca67efffa427bac0989b2b
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-09-27 23:13:08 +0200

    finish note remake and add comments

--- examples/src/App.jsx
+++ examples/src/App.jsx
@@ -3,6 +3,34 @@ import Note from './Components/Note'
 
 function App(props) {
   const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+
+  const addNote = (e) => {
+    e.preventDefault()
+    // prevent the default submission which causes the page to refresh
+    const noteObj = {
+      content: newNote,
+      important: Math.random < 0.5,
+      id: String(notes.length + 1)
+    }
+    // this is the contents of the newly created note
+
+    setNotes(notes.concat(noteObj))
+    /* 
+    The method does not mutate the original notes array, but rather 
+    creates a new copy of the array with the new item added 
+    to the end. This is important since we must never mutate 
+    state directly in React!
+    */
+    //here we concat/add the new note to the existing notes array
+    setNewNote('')
+    //turns the input field blank after submission
+  }
+
+  const handleNoteChange = (e) => {
+    console.log(e.target.value)
+    setNewNote(e.target.value)
+  }
 
   return (
     <div>
@@ -13,6 +41,11 @@ function App(props) {
           <Note key={note.id} note={note} />
         )}
       </ul>
+
+      <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange}/>
+      <button type="submit">save</button>
+      </form>
     </div>
   )
 }
