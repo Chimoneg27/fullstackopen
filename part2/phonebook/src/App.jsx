0b4f3d7410e57a0be79bed23b66561fc7d53a1e8
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-09-29 17:42:48 +0200

    finish logic for adding a name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,42 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas'}
+  ])
+  const [newName, setNewName] = useState('')
 
+  const submitName = (e) => {
+    e.preventDefault()
+    setPersons(persons.concat({
+      name: newName
+    }))
+    setNewName('')
+  }
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value)
+  }
+  console.log(persons)
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <form>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+
+        <div>
+          <button type="submit" onClick={submitName}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        </div>
     </div>
   )
 }
