6850374052f820b99418a039ae5fe261bf61c4a4
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-10-02 15:21:11 +0200

    fix form component

--- part2/phonebook/src/components/Form.jsx
+++ part2/phonebook/src/components/Form.jsx
@@ -1,19 +1,21 @@
-const Note = ({ name, number, onChange, onClick }) => {
-  <form>
-    <div>
-      name: <input value={name} onChange={onChange} />
-    </div>
+const Note = ({ name, number, onChangeName, onChangeNumber, onClick }) => {
+  return (
+    <form>
+      <div>
+        name: <input value={name} onChange={onChangeName} />
+      </div>
 
-    <div>
-      number: <input value={number} onChange={onChange} />
-    </div>
+      <div>
+        number: <input value={number} onChange={onChangeNumber} />
+      </div>
 
-    <div>
-      <button type="submit" onClick={onClick}>
-        add
-      </button>
-    </div>
-  </form>
+      <div>
+        <button type="submit" onClick={onClick}>
+          add
+        </button>
+      </div>
+    </form>
+  )
 }
 
 export default Note
