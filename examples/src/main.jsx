fe56c892adeb3d95f9c81b20ac1641c5e008574d
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-10-04 00:22:38 +0200

    render notes from the json file

--- examples/src/main.jsx
+++ examples/src/main.jsx
@@ -1,32 +1,8 @@
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import axios from 'axios'
-import App from './App.jsx'
+import App from './App'
 
-axios
-  .get('http://localhost:3001/notes')
-  .then(response => {
-    const notes = response.data
-    console.log(notes)
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
 })
-
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
-
-createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
