6611e49cb2bc5404caa199a56da05e84d9f8e116
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-10-07 00:40:07 +0200

    finish PUT method

--- examples/src/App.jsx
+++ examples/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 
     const noteObj = {
       content: newNote,
-      important: Math.random() < 0.5,
+      important: Math.random() > 0.5,
     }
     
     axios
@@ -33,6 +33,26 @@ const App = () => {
 
   const handleNoteChange = (e) => {
     setNewNote(e.target.value)
+  }
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    // Here we use the find() array method to target the note with the specific id
+  
+    const changedNote = { ...note, important: !note.important }
+    // Here we create a new note object with the same properties as the original note,
+    // but with the 'important' property toggled (true to false or vice-versa)
+  
+    axios.put(url, changedNote).then(response => {
+      // The put method sends a request to update the note at the specified URL with the changedNote object
+      // Once the request is successful, we update our local state
+  
+      setNotes(notes.map(n => n.id !== id ? n : response.data))
+      // We use the map() array method to create a new array of notes
+      // If the note's id does not match the id we are toggling, we keep it unchanged
+      // If the note's id matches, we replace it with the updated note from the server response
+    })
   }
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
@@ -48,7 +68,11 @@ const App = () => {
 
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note 
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
 
