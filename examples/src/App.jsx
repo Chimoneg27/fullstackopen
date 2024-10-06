c3a8f76a49f240ebab5fc450d28cbb19d614d853
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-10-06 23:11:58 +0200

    remove cosole logs

--- examples/src/App.jsx
+++ examples/src/App.jsx
@@ -8,11 +8,9 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    console.log('effect')
     axios
       .get('http://localhost:3001/notes')
       .then(response => {
-        console.log('promise fulfilled')
         setNotes(response.data)
       }, [])
   })
