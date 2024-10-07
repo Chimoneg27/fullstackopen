3948393ab8e0d2d137d0b4e87d8b7f00883632e6
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-10-07 16:23:57 +0200

    update a component

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,10 +1,11 @@
-const Persons = ({ personsArray }) => {
+const Persons = ({ personsArray, removePerson }) => {
   return (
     <ul>
       {personsArray.map((person) => {
         return (
           <li key={person.name}>
             {person.name} {person.number}
+            <button onClick={removePerson}>delete</button>
           </li>
         )
       })}
