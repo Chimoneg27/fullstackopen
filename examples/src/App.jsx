55c95f595cefb57b88413761298d53ee9bbfa6b0
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-10-10 22:38:00 +0200

    add footer to notes app

--- examples/src/App.jsx
+++ examples/src/App.jsx
@@ -4,6 +4,21 @@ import Note from './Components/Note'
 import noteService from './services/notes'
 import Notification from './Components/Notification'
 
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+
+  return (
+    <div style={footerStyle}>
+    <br/>
+    <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+    </div>
+  )
+}
+
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
@@ -85,6 +100,8 @@ const App = () => {
       <input value={newNote} onChange={handleNoteChange}/>
       <button type="submit">save</button>
       </form>
+
+      <Footer />
     </div>
   )
 }
