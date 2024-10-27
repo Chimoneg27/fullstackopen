701710b359d5c75ef9c8cf887db024ac698dc57b
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-10-27 18:30:48 +0200

    add icons to images component

--- part2/dataforcountries/src/Components/Weather.jsx
+++ part2/dataforcountries/src/Components/Weather.jsx
@@ -1,9 +1,16 @@
 const Weather = ({ obj }) => {
+    const iconUrl = " https://openweathermap.org/img/wn/"
+    const image = obj.weather[0].icon +".png"
+    const src = iconUrl + image
     return (
-      <div>
+      <div className="weatherIcon">
         <h2>Weather in {obj.name}</h2>
 
-        <p>temperature {obj.main.temp}</p>
+        <img src={src} alt="weatherIcon" />
+
+        <p>temperature {obj.main.temp} degrees</p>
+
+        <p>wind {obj.wind.speed} m/s</p>
       </div>
     )
 }
