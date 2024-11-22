9ec7b6ba2b906f65c78c806b49d4e97675c11057
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-23 00:13:42 +0200

    add error handling

--- backend1/index.js
+++ backend1/index.js
@@ -19,22 +19,25 @@ app.use(express.static('dist'))
   })
 
   app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    response.json(note)
-
-    if(note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  })
-
-  app.delete('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-      response.json(note)
+    Note.findById(request.params.id)
+    .then(note => {
+      if(note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
     })
   })
+
+  // app.delete('/api/notes/:id', (request, response) => {
+  //   Note.findById(request.params.id).then(note => {
+  //     response.json(note)
+  //   })
+  // })
 
   app.post('/api/notes', (request, response) => {
     const body = request.body
