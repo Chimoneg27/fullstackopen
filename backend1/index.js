feab80d3706d198d9995a6a45a5a041f8a93dfea
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-26 22:39:44 +0200

    update errorHandler function

--- backend1/index.js
+++ backend1/index.js
@@ -38,7 +38,7 @@ app.delete('/api/notes/:id', (request, response) => {
   .catch(error => next(error))
 })
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body
 
   if (body.content === undefined) {
@@ -53,6 +53,7 @@ app.post("/api/notes", (request, response) => {
   note.save().then((savedNote) => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
 app.put("/api/notes/:id", (request, response) => {
@@ -81,6 +82,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
