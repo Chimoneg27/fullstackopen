4ee91e5f10ef2ed74edd658308609e6ca9d382b0
Chimoneg27 <chimonegarvin27@gmail.com>
2024-12-02 22:35:13 +0200

    update files

--- backend1/models/note.js
+++ backend1/models/note.js
@@ -1,28 +1,15 @@
-const mongoose = require("mongoose")
-mongoose.set("strictQuery", false)
-
-const url = process.env.MONGODB_URI
-console.log("connecting to", url)
-
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB")
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message)
-  })
+const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
-  important: Boolean
+  important: Boolean,
 })
 
-noteSchema.set("toJSON", {
+noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
@@ -30,4 +17,4 @@ noteSchema.set("toJSON", {
   }
 })
 
-module.exports = mongoose.model("Note", noteSchema)
+module.exports = mongoose.model('Note', noteSchema)
