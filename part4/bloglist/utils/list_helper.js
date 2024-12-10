08216c7a6e5e96666d84c4cbddddb0daeba918f4
Chimoneg27 <chimonegarvin27@gmail.com>
2024-12-10 22:51:20 +0200

    start list of helper functions

---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,9 @@
+// const { test, describe } = require('node:test')
+// const assert = require('node:assert')
+const dummy = (blogs) => {
+  return 1 + (blogs.length - blogs.length)
+}
+
+module.exports = {
+  dummy
+}
