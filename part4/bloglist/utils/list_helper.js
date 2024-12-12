6e23fd1aec992d9b34a216c69210bb49af33ff4e
Chimoneg27 <chimonegarvin27@gmail.com>
2024-12-12 22:56:14 +0200

    add function to add likes

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1 + (blogs.length - blogs.length)
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0 ?
+    0 : blogs.reduce((sum, post) => {
+      return sum + post.likes
+    }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
