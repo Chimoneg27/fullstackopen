3937bc02bf9677bc9bdfc70a532994653a9e4945
Chimoneg27 <chimonegarvin27@gmail.com>
2024-12-22 01:04:12 +0200

    use asyn/await and write two new tests

--- backend1/tests/note_api.test.js
+++ backend1/tests/note_api.test.js
@@ -12,6 +12,19 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+
+  assert.strictEqual(response.body.length, 2)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(e => e.content)
+  assert(contents.includes('HTML is easy'))
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
