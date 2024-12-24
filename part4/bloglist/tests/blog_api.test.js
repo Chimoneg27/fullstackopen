13fe81c5acf38271169af678f0841594d7b87fbe
Chimoneg27 <chimonegarvin27@gmail.com>
2024-12-25 00:00:10 +0200

    reset database before each test

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,9 +1,33 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'This is test one blog',
+    author: 'Stella Queresma',
+    url: 'howdoesdinner@8sound.com',
+    likes: 2
+  },
+  {
+    title: 'I am a billionaire',
+    author: 'Garvin Chimone',
+    url: 'leaveyourmark@now.com',
+    likes: 27
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObj = new Blog(initialBlogs[0])
+  await blogObj.save()
+  blogObj = new Blog(initialBlogs[1])
+  await blogObj.save()
+})
 
 test('notes are returned as json', async () => {
   await api
