07f1fd786d87919e06aa34c15d8d496054c940a6
Chimoneg27 <chimonegarvin27@gmail.com>
2024-12-29 19:47:05 +0200

    add route for updating the blog

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -27,4 +27,26 @@ blogsRouter.delete('/:id', async (request, response) => {
   response.status(204).end()
 })
 
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+  const blogToUpdate =  await Blog.findByIdAndUpdate(
+    request.params.id,
+    blog,
+    { new: true, runValidators: true }
+  )
+
+  if (!blogToUpdate) {
+    return response.status(404).json({ error: 'blog not found' })
+  }
+
+  response.json(blogToUpdate)
+})
+
 module.exports = blogsRouter
