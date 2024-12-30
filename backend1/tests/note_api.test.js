8e838ef5e4ffbd6df9a103e1b446c9c643d289c6
Chimoneg27 <chimonegarvin27@gmail.com>
2024-12-30 20:46:35 +0200

    write test for creating a new user

--- backend1/tests/note_api.test.js
+++ backend1/tests/note_api.test.js
@@ -1,5 +1,7 @@
 const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 const supertest = require('supertest')
 const mongoose = require('mongoose')
 
@@ -122,6 +124,38 @@ describe('when there are some notes saved initially', () => {
   })
 })
 
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    assert(usernames.includes(newUser.username))
+  })
+})
 
 after(async () => {
   await mongoose.connection.close()
