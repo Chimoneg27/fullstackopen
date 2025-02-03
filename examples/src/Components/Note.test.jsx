d7b1a9b197e1202f3b822e15d03afb51ffd9ac05
Chimoneg27 <chimonegarvin27@gmail.com>
2025-02-03 13:35:36 +0200

    write tests for button clicks

--- examples/src/Components/Note.test.jsx
+++ examples/src/Components/Note.test.jsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
+import { expect } from 'vitest'
 
 test('renders content', () => {
   const note = {
@@ -7,10 +9,31 @@ test('renders content', () => {
     important: true
   }
 
-  const { container } = render(<Note note={note} />)
+  render(<Note note={note} />)
 
-  const div = container.querySelector('.note')
-  expect(div).toHaveTextContent(
-    'Component testing is done with react-testing-library'
+  const element = screen.getByText('Component testing is done with react-testing-library')
+
+  screen.debug(element)
+
+  expect(element).toBeDefined()
+})
+
+test('clicking the button calls the event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+  //mock functiion defined with vitest
+  const mockhandler = vi.fn()
+
+  render(
+    <Note note={note} toggleImportance={mockhandler}/>
   )
+  // a session is started to interact with the rendered component
+  const user = userEvent.setup()
+
+  const button = screen.getByText('make not important')
+  await user.click(button) // clicking happens with the method click of the userEvent-library
+
+  expect(mockhandler.mock.calls).toHaveLength() // toHaveLength verifies that the mock function has been called exactly once
 })
