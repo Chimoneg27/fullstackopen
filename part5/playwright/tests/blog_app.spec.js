75086e4ddfc8f7c6cd14f8a642d396952fbebd4e
Chimoneg27 <chimonegarvin27@gmail.com>
2025-02-19 09:53:33 +0200

    finish exercise 5.17

--- part5/playwright/tests/blog_app.spec.js
+++ part5/playwright/tests/blog_app.spec.js
@@ -1,9 +1,15 @@
-const { test, expect } = require('@playwright/test')
+const { test, expect, describe, beforeEach } = require('@playwright/test')
 
-test('front page can be opened', async ({ page }) => {
-  await page.goto('http://localhost:5173')
+describe('BlogList App is shown', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
 
-  const locator = await page.getByText('Username')
-  await expect(locator).toBeVisible()
-  await expect(page.getByText('Password')).toBeVisible()
+  test('show login form', async ({ page }) => {
+    const loginForm = page.locator('form') // get the form element
+    await expect(loginForm).toBeVisible()
+    await expect(loginForm.locator('input[name="username"]')).toBeVisible()
+    await expect(loginForm.locator('input[name="password"]')).toBeVisible()
+    await expect(loginForm.locator('button[type="submit"]')).toBeVisible()   
+  })
 })
