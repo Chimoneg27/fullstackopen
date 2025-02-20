d6b452c161a68638288644047a90705c075cb3d0
Chimoneg27 <chimonegarvin27@gmail.com>
2025-02-20 22:25:33 +0200

    finsih exercise 5.18

--- part5/playwright/tests/blog_app.spec.js
+++ part5/playwright/tests/blog_app.spec.js
@@ -1,7 +1,17 @@
 const { test, expect, describe, beforeEach } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('BlogList App is shown', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3002/api/testing/reset')
+    await request.post('http://localhost:3002/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -11,5 +21,17 @@ describe('BlogList App is shown', () => {
     await expect(loginForm.locator('input[name="username"]')).toBeVisible()
     await expect(loginForm.locator('input[name="password"]')).toBeVisible()
     await expect(loginForm.locator('button[type="submit"]')).toBeVisible()   
+  })
+
+  test('login fails with wrong password', async ({page}) => {
+    await loginWith(page, 'mluukai', 'garvin')
+    await expect(page.getByText('Wrong username or password')).toBeVisible()
+  })
+
+  describe('when logged in', () => {
+    test('user can log in', async ({ page }) => {
+      await loginWith(page, 'mluukkai', 'salainen')
+      await expect(page.getByText('Matti Luukkainen')).toBeVisible()
+    })
   })
 })
