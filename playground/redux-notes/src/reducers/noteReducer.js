8c4715adfb1b9120de184c3d76fce293e3b33e5d
Chimoneg27 <chimonegarvin27@gmail.com>
2025-03-17 15:34:33 +0200

    remove unused App component from noteReducer file

--- playground/redux-notes/src/reducers/noteReducer.js
+++ playground/redux-notes/src/reducers/noteReducer.js
@@ -30,18 +30,4 @@ store.dispatch({
   },
 });
 
-const App = () => {
-  return (
-    <div>
-      <ul>
-        {store.getState().map((note) => (
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? "important" : ""}</strong>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
 export default noteReducer;
