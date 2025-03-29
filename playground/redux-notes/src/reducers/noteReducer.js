afa553659a1dd5c892bd09ed348d649a9cdc0eb4
Chimoneg27 <chimonegarvin27@gmail.com>
2025-03-29 14:27:57 +0200

    send data to the backend

--- playground/redux-notes/src/reducers/noteReducer.js
+++ playground/redux-notes/src/reducers/noteReducer.js
@@ -1,32 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// const initialState = [
-//   {
-//     content: 'reducer defines how redux store works',
-//     important: true,
-//     id: 1,
-//   },
-//   {
-//     content: 'state of store can contain any data',
-//     important: false,
-//     id: 2,
-//   },
-// ]
-
-const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
-
 const noteSlice = createSlice({
   name: 'notes', // prefix which is used in the action's type values. this will later be notes/createNote
   initialState: [], // defines the reducers initial state
   reducers: {  //  this parameter takes the reducers itself as an object
     createNote(state, action) {
-      const content = action.payload
-      state.push({
-        content,
-        important: false,
-        id: generateId(),
-      })
+      state.push(action.payload)
     },
     toggleImportanceOf(state, action) {
       const id = action.payload
