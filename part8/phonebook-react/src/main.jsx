dbeb778fc5fbbd046aa41148b6d70b9277b6a8ad
Chimoneg27 <chimonegarvin27@gmail.com>
2025-05-25 21:11:53 +0200

    install and use Apolloclient

--- part8/phonebook-react/src/main.jsx
+++ part8/phonebook-react/src/main.jsx
@@ -3,8 +3,41 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { 
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  ApolloProvider
+} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+const query = gql`
+  query {
+    allPersons {
+      name,
+      phone,
+      address {
+        street,
+        city
+      }
+      id
+    }
+  }
+`
+
+client.query({ query }) // client line 8 creates client object which is used to query the server
+  .then((response) => {
+    console.log(response.data)
+  })
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 )
