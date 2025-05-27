54724113d364a24b3365b81cd1995a33aea94baa
Chimoneg27 <chimonegarvin27@gmail.com>
2025-05-27 14:02:12 +0200

    complete exercise 8.8

--- part8/library-frontend/src/components/Authors.jsx
+++ part8/library-frontend/src/components/Authors.jsx
@@ -1,10 +1,32 @@
-const Authors = () => {
- console.log('Authors component rendered');
-  // const authors = []
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from '../services/queries'
 
-  return (
+const Authors = () => {
+  const authors = useQuery(ALL_AUTHORS)
+
+  if (authors.loading) {
+    return <div>loading...</div>
+  }
+
+return (
     <div>
       <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.data.allAuthors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
