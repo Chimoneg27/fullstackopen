f84bfced2b28823fc5bcc496378e719568ba4089
Chimoneg27 <chimonegarvin27@gmail.com>
2025-05-27 13:17:45 +0200

    add react router

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -1,25 +1,27 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
 
 const App = () => {
-  const [page, setPage] = useState("authors");
-
   return (
-    <div>
-      <div>
-        <button onClick={() => setPage("authors")}>authors</button>
-        <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
-      </div>
+      <Router>
+        <div>
+          <Link to="/"><button>Authors</button></Link>
+          <Link to="/books"><button>Books</button></Link>
+          <Link to="/add"><button>Add Book</button></Link>
+        </div>
 
-      <Authors show={page === "authors"} />
-
-      <Books show={page === "books"} />
-
-      <NewBook show={page === "add"} />
-    </div>
+        <Routes>
+          <Route path="/" element={<Authors />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/add" element={<NewBook />} />
+        </Routes>
+      </Router>
   );
 };
 
