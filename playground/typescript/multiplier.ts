da6b8502dbaac5e8bef2d8b335be2c87b3f7d581
Chimoneg27 <chimonegarvin27@gmail.com>
2025-06-27 19:48:54 +0200

    add my first types

--- playground/typescript/multiplier.ts
+++ playground/typescript/multiplier.ts
@@ -1,5 +1,5 @@
-const multiplicator = (a, b, printText) => {
+const multiplicator = (a: number, b: number, printText: string) => {
   console.log(printText, a * b);
 }
 
-multiplicator('how about a string?', 4, 'Multiplied a string and 4, the result is:');
+multiplicator( 3 , 4, 'Multiplied a string and 4, the result is:');
